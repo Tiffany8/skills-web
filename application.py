fd6b343a7cdfdc8d38a140ea79bc58176f879f0e
Tiffany <tiffany@TRWilliams.local>
2015-04-27 09:54:21 -0700

    The furthest I was able to get before submitting to my advisor.  The first two exercises were completed; however, I was unable to get through the js-skills.html.  I got up to the Advanced problems.  I had a few issues with some of the problems.

--- application.py
+++ application.py
@@ -1,11 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index_page():
-    # Return this as a strange
+   
+   
     return render_template("application-form.html")
+
+@app.route("/application", methods=["POST", "GET"])
+def application():
+	firstname = request.form["firstname"]
+	lastname = request.form["lastname"]
+	salary = request.form["salary"]
+	position = request.form["position"]
+	return render_template("application.html", firstname=firstname, lastname=lastname, salary=salary, position=position)
+	# , firstname=firstname, lastname=lastname, salary=salary, position=position
+
+    
 
     # Alternately, we could make this a Jinja template in `templates/`
     # and return that result of rendering this, like:
